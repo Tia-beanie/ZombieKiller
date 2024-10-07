523646724e3267eb9a3fe7dca845f859583b3291
Tia-beanie <144304264+Tia-beanie@users.noreply.github.com>
2024-10-06 17:42:38 -0700

    Add files via upload

---
+++ ZombieKiller.java
@@ -0,0 +1,18 @@
+public class ZombieKiller {
+    public static void main(String[] args){
+        System.out.println("Welcome to our zombie killer program!!!");
+
+        String backpack[] = {"Shotgun", "Assualt Rifle", "Sniper"};
+        String zombies[] = {"Close-range zombie", "Mid-range zombie", "Long-range zombie"};
+
+        System.out.println("Backpack items!");
+        System.out.println(backpack[0]);
+        System.out.println(backpack[0]);
+        System.out.println(backpack[0]);
+        System.out.println(backpack[2]);
+    
+        int numbersZombiesHATE[] = {4, 90, 70, 123, 12, 4, 561, 1};
+
+        System.out.println((numbersZombiesHATE[6]));
+    }
+}
